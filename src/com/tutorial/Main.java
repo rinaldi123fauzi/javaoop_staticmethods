448fbf70dbc85905768c7d98922c32b16efbb945
rinalfauzi <rinalfauzi@gmail.com>
2020-04-08 07:03:34 +0700

    First push

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,42 @@
+package com.tutorial;
+
+import java.util.ArrayList;
+
+class Player{
+
+    private static int numberOfLayer;
+    private static ArrayList<String> nameList = new ArrayList<String>();
+    private String name;
+
+    //Construction
+    Player(String name){
+        this.name = name;
+        Player.numberOfLayer++;
+        Player.nameList.add(this.name);
+    }
+
+    void show(){
+        System.out.println("Player name = " + this.name);
+    }
+
+    //ini adalah static method
+    static void getNumberOfPlayer(){
+        System.out.println("Number of Player : " + Player.numberOfLayer);
+    }
+
+    static ArrayList<String> getNames(){
+        return Player.nameList;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Player player1 = new Player("Saitama");
+        Player player2 = new Player("All Mighty");
+        Player player3 = new Player("Mi Lady");
+        Player player4 = new Player("Midnight");
+
+        Player.getNumberOfPlayer();
+        System.out.println("Names : " + Player.getNames());
+    }
+}
